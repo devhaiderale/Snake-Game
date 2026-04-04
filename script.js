7c8898d088c535e994b048d85ab1bbde84a4a4d4
Haider Ali <devhaiderale@gmail.com>
2026-04-05 00:08:06 +0500

    js basic logic done

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const board = document.querySelector('#board');
-const blockheight = 30
-const blockwidth = 30
+const blockheight = 50
+const blockwidth = 50
 
 const cols = Math.floor(board.clientWidth / blockwidth);
 const rows = Math.floor(board.clientHeight / blockheight);
 
-for(let i = 0; i<rows * cols; i++){
-    const block = document.createElement('div');
-    block.classList.add('block');
-    board.appendChild(block);
+for(let row = 0; row<rows; row++){
+    for(let col = 0; col<cols; col++){
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+    }
 }
