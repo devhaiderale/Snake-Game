7893e3c653a6d106a8036d3d35c08a727ca6910d
Haider Ali <devhaiderale@gmail.com>
2026-04-05 14:51:47 +0500

    just pushing

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const blockwidth = 45
 
 const blocks = []
 const snake = [
-    { x: 1, y: 3 },
-    { x: 1, y: 4 },
-    { x: 1, y: 5 }
+    { x: 1, y: 3 }
 ]
-let direction = 'left'
+let direction = 'right'
 
 const cols = Math.floor(board.clientWidth / blockwidth);
 const rows = Math.floor(board.clientHeight / blockheight);
@@ -30,9 +28,19 @@ function render() {
 }
 
 setInterval(() => {
+    setInterval(() => {
+        direction = 'right'
+    }, 1000);
     let head = null
     if (direction === 'left') {
         head = { x: snake[0].x, y: snake[0].y - 1 }
     }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill');
+    })
+
+    snake.unshift(head);
+    snake.pop();
     render();
-}, 300);
+}, 500);
